2451513a8568a133f20786330eca4afa17e55e31
Jay McCarthy <jay.mccarthy@gmail.com>
2014-11-22 10:54:45 -0800

    Deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,11 @@
 #lang info
 (define collection "lux")
-(define deps '("base"
+(define deps '("draw-lib"
+               "drracket"
+               "gui-lib"
+               "htdp-lib"
+               "pict-lib"
+               "base"
                "rackunit-lib"))
 (define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/lux.scrbl" ())))
